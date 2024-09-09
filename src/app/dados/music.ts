a9e4b842d8812b9e5e773bec2a134f72cdc01f49
Pedro-Pesssoa <pedropessoacaeira@gmail.com>
2024-09-09 13:11:31 -0300

    tarefa 2

---
+++ src/app/dados/music.ts
@@ -0,0 +1,29 @@
+// dados/music.ts
+type Music = {
+    name: string;
+    author: string;
+    description: string;
+    urlAudio: string;
+  };
+  
+  export const musics: Music[] = [
+    {
+      name: "Musica 01",
+      author: "Ruan Plabblo",
+      description: "Autor 02",
+      urlAudio: "audios/audio1.mp3",
+    },
+    {
+      name: "Musica 02",
+      author: "João Felepe",
+      description: "Autor 02",
+      urlAudio: "audios/audio2.mp3",
+    },
+    {
+      name: "Musica 03",
+      author: "Antonio Caue",
+      description: "Autor 02",
+      urlAudio: "audios/audio3.mp3",
+    },
+  ];
+  
